0af3f228a014c1f5cfd595b73bb5477031f8f002
davit <davitiani.dato@gmail.com>
2019-03-04 23:51:07 +0400

    delete component completed

--- JS-Study/budgety/app.js
+++ JS-Study/budgety/app.js
@@ -60,6 +60,21 @@ var budgetController = (function () {
 
        },
 
+       deleteItem: function(type, id) {
+            var ids, index;  
+
+            ids = data.allItems[type].map(function(current) {
+                return current.id;
+            });
+
+            index = ids.indexOf(id);
+
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1);
+            }
+
+       },
+
        calculateBudget: function() {
 
             // calculate total income and expences
@@ -146,6 +161,11 @@ var UIController = (function() {
             // insert html into the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
 
+        },
+
+        deleteListItem: function(selectorID) {
+            var el = document.getElementById(selectorID);
+            el.parentNode.removeChild(el);
         },
 
         clearFields: function() {
@@ -251,14 +271,14 @@ var controller = (function(budgetCtrl, UICtrl) {
         if(itemID) {
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitID[1];
+            ID = parseInt(splitID[1]);
 
             //1. delete the item from the data structure
-
+            budgetCtrl.deleteItem(type, ID);
             //2. delete item from the ui
-
+            UICtrl.deleteListItem(itemID)
             //3. update and show the new budget
-
+            updateBudget();
         }
 
     }
