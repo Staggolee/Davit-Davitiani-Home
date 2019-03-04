eb9a92705e7ce773c1d2a924866c373542180e3e
davit <davitiani.dato@gmail.com>
2019-03-05 02:25:24 +0400

    added date

--- JS-Study/budgety/app.js
+++ JS-Study/budgety/app.js
@@ -158,7 +158,8 @@ var UIController = (function() {
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expencesPercLabel: '.item__percentage'
+        expencesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
     };
 
     var formatNumber = function(num, type) {
@@ -282,6 +283,19 @@ var UIController = (function() {
 
         },
 
+        displayMonth: function() {
+            var now, year, months, month;
+
+            now = new Date();
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+             'September', 'October', 'November', 'December'];
+            month = now.getMonth();
+            
+            year = now.getFullYear();
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year; 
+
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -386,6 +400,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('Allpicatipn has started'); 
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
